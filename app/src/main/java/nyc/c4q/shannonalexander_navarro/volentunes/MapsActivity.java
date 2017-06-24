c943fbf83e5e9ab884b8dbbffde53494ddafec26
Leigh Douglas <leighcdouglas1@gmail.com>
2017-06-24 17:18:57 -0400

    Created map and volunteer info page

--- app/src/main/java/nyc/c4q/shannonalexander_navarro/volentunes/MapsActivity.java
+++ app/src/main/java/nyc/c4q/shannonalexander_navarro/volentunes/MapsActivity.java
@@ -1,13 +1,16 @@
 package nyc.c4q.shannonalexander_navarro.volentunes;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -39,8 +42,34 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng art57 = new LatLng(40.79937645675807, -73.96519802101764);
+        LatLng consumerhanover = new LatLng(40.7043068313896, -74.00918226342714);
+        LatLng homeworkHelp = new LatLng(40.79013250049554, -73.94275604693821);
+        LatLng bikeValet = new LatLng(40.601149489516224, -73.99944293492823);
+        LatLng broome = new LatLng(40.68457809700163, -73.8496016684822);
+        LatLng location1 = new LatLng(40.73551330580631, -73.98834497200824);
+        LatLng location2 = new LatLng(40.63454489394634, -74.02560091774434);
+
+        CameraUpdate zoom = CameraUpdateFactory.newLatLngZoom(art57, 11);
+        mMap.addMarker(new MarkerOptions().position(art57).title("Marker in Sydney"));
+        mMap.addMarker(new MarkerOptions().position(consumerhanover).title("Marker in Sydney"));
+        mMap.addMarker(new MarkerOptions().position(homeworkHelp).title("Marker in Sydney"));
+        mMap.addMarker(new MarkerOptions().position(bikeValet).title("Marker in Sydney"));
+
+        mMap.addMarker(new MarkerOptions().position(broome).title("Marker in Sydney"));
+
+        mMap.addMarker(new MarkerOptions().position(location1).title("Marker in Sydney"));
+        mMap.addMarker(new MarkerOptions().position(location2).title("Marker in Sydney"));
+
+        mMap.moveCamera(zoom);
+
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                Intent intent = new Intent(getApplicationContext(), EventDetailsActivity.class);
+                startActivity(intent);
+                return false;
+            }
+        });
     }
 }
