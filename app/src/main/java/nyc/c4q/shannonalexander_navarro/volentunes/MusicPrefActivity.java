0398b0d45af21a02ee35f9f34dd53c37777000dc
Shannon Alexander-Navarro <shannonalexander@hotmail.com>
2017-06-24 16:32:58 -0400

    activities

---
+++ app/src/main/java/nyc/c4q/shannonalexander_navarro/volentunes/MusicPrefActivity.java
@@ -0,0 +1,31 @@
+package nyc.c4q.shannonalexander_navarro.volentunes;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MusicPrefActivity extends AppCompatActivity {
+
+     Button saveBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music_pref);
+
+        initViews();
+    }
+
+    private void initViews(){
+        saveBtn = (Button) findViewById(R.id.saveMusicBtn);
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MusicPrefActivity.this, VolunteerPrefActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
